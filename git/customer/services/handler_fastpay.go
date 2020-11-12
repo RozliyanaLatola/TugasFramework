b32e6b6086913862ec10eb15ec8e1041218a553e
Alhamdulillah <rozliyanalatola@gmail.com>
2020-11-12 18:37:24 +0700

    first commit

---
+++ git/customer/services/handler_fastpay.go
@@ -0,0 +1,17 @@
+package services
+
+import (
+	"context"
+
+	cm "FrameworkLyn/git/customer/common"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func (PaymentService) FastPayHandler(ctx context.Context, req cm.FastPayRequest) (res cm.FastPayResponse) {
+
+	
+
+	return
+}
